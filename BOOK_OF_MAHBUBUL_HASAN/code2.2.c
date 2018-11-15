185772a4294f0288f878af665f8281f3059363c2
Sabbir Ahmed <sabbir12003@gmail.com>
2018-11-16 00:14:13 +0600

    page 20:simple input

---
+++ BOOK_OF_MAHBUBUL_HASAN/code2.2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+
+    scanf("%d%d",&a,&b);
+
+    printf(" addition of two number=%d\n subtraction of two number=%d\n",a+b,a-b);
+
+    return 0;
+}
