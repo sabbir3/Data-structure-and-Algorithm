4858b5240062b173f516f1fe44d44e8a869e7b71
Sabbir Ahmed <sabbir12003@gmail.com>
2018-11-16 20:02:12 +0600

    page 23: area of a circle

---
+++ BOOK_OF_MAHBUBUL_HASAN/code2.3.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double r,area,pi;
+
+    scanf("%lf",&r);
+    pi=acos(-1.);
+    printf("pi=%lf\n",pi);
+    area=pi*r*r;
+
+    printf("%lf",area);
+
+    return 0;
+}
