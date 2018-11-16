c3f97c2f2c01ff0f3418831f9acef767750dbbf5
Sabbir Ahmed <sabbir12003@gmail.com>
2018-11-16 23:46:00 +0600

    page 24:perimeter of circle

---
+++ BOOK_OF_MAHBUBUL_HASAN/exercise_finding_perimeter_of_circle.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double radius,perimeter;
+    cout<<"enter the radius=";
+    cin>>radius;
+    cout<<"\n";
+
+    perimeter=2*M_PI*radius;
+
+    cout<<"perimeter of the circle="<<perimeter;
+
+    return 0;
+}
