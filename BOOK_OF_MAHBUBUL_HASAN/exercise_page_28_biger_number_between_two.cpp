a2ec0ed6663c5f3d706cbf055784cab6b2ba4121
Sabbir Ahmed <sabbir12003@gmail.com>
2018-11-17 04:08:39 +0600

    page 28: bigger number between two number

---
+++ BOOK_OF_MAHBUBUL_HASAN/exercise_page_28_biger_number_between_two.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,t;
+    cout<<"enter a and b=";
+    cin>>a>>b;
+    cout<<"\n";
+
+    if(a>b){
+        cout<<"the bigger number is a="<<a;
+    }
+    else{
+        cout<<"the bigger number is b="<<b;
+    }
+    return 0;
+}
