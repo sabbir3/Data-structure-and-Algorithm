a850efac27a7571cbacf1231ed919cb1d3fc5fca
Sabbir Ahmed <sabbir12003@gmail.com>
2018-11-16 23:57:01 +0600

    closest integer number of a given number

---
+++ BOOK_OF_MAHBUBUL_HASAN/exercise_ceiling_of_squre_root.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double number ;
+
+    cin>>number;
+
+    cout<<"closest integer number of a given number="<<ceil(sqrt(number));
+
+    return 0;
+}
