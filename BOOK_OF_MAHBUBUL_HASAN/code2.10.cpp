8608ff96022564589c53b8f46346517e2c6e6a68
Sabbir Ahmed <sabbir12003@gmail.com>
2018-11-18 19:17:08 +0600

    page 30:simple loop 3

---
+++ BOOK_OF_MAHBUBUL_HASAN/code2.10.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,a;
+
+    for(i=1; i<=10; i++){
+    if(i%2==0) continue;
+    cout<<i<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+
+    for(i=1; i<=10; i++){
+
+        if(i>3) break;
+        cout<<i<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+
+    while( scanf("%d",&a)!=EOF){
+
+        if(a==0) break;
+        cout<<a<<endl;
+    }
+
+    return 0;
+}
