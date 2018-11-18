877121b4ad0bfa0ca0b4a313379ca91d0d2e5519
Sabbir Ahmed <sabbir12003@gmail.com>
2018-11-18 17:55:36 +0600

    page 28: simple loop

---
+++ BOOK_OF_MAHBUBUL_HASAN/code2.8.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+
+    for(i=1; i<=10; i++){
+        cout<<i<<endl;
+    }
+    cout<<"\n";
+
+    for(i=10; i>=1; i--){
+
+        cout<<i<<endl;
+    }
+    cout<<"\n";
+
+     for(i=1; i<=10; i+=2){
+
+        cout<<i<<endl;
+    }
+
+      cout<<"\n";
+    j=5;
+
+    while(j<=7){
+
+        cout<<j<<endl;
+        j++;
+    }
+
+    return 0;
+}
+
