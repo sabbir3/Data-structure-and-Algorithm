0842d5f0e4b7e40505adb6dbef87eec0b04f88f1
Sabbir Ahmed <sabbir12003@gmail.com>
2018-11-18 23:37:21 +0600

    page 32: 1 + 2 + 3.....+n

---
+++ BOOK_OF_MAHBUBUL_HASAN/exercise_page_32_1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,n;
+
+    cin>>n;
+
+   /* for(i=1; i<=n; i++){
+
+        cout<<((i*(i+1))/2)<<endl;
+    }
+    return 0;
+
+    OR*/
+
+    for(i=1; i<=n; i++){
+
+        cout<<i<<"+";
+    }
+    return 0;
+
+}
