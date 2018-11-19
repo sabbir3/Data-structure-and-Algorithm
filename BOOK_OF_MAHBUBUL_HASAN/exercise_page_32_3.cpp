bff1b4032e91c17ed7953cf273534da12da475cb
Sabbir Ahmed <sabbir12003@gmail.com>
2018-11-19 23:32:25 +0600

    1^1+2^2+3^3+....+n^n

---
+++ BOOK_OF_MAHBUBUL_HASAN/exercise_page_32_3.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n,sum=0;
+    cin>>n;
+    for(i=1; i<=n; i++)
+    {
+        sum+=pow(i,i);
+    }
+    cout<<sum<<endl;
+     return 0;
+}
